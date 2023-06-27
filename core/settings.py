0eb2b1457ae90f3085a8c781ac998130543f1562
alita <alita.avs@gmail.com>
2023-06-27 22:01:51 +0300

    settings

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = ['alitavs.pythonanywhere.com']
 
 INTERNAL_IPS = [
     "127.0.0.1",
